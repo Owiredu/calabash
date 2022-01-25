9342dcbfb46070c72214839cf8e918bbfe15d227
Nana Kofi Owiredu <owiredu@localhost.localdomain>
2022-01-25 00:53:48 +0000

    Changed App.php to index.php; added set_cookies method

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// require modules
+require_once(__DIR__ . '/lib/core/Calabash.php');
+require_once(__DIR__ . '/routes/UserRouter.php');
+
+// use interfaces
+use Lib\Core\Calabash as App;
+use Router\UserRouter as UserRouter;
+
+// initialize application
+$app = new App();
+
+// set cookies
+$app->set_cookies(
+    $name = "calabash",
+    $value = uniqid("calabash_", $more_entropy = true),
+    $options = [
+        'expires' => time() + 60 * 60 * 24 * 30, // expires after 30 days
+        'path' => '/calabash',
+        'domain' => 'localhost', // eg. '.example.com', leading dot for compatibility or use subdomain
+        'secure' => true,     // or false
+        'httponly' => true,    // or false
+        'samesite' => 'None' // None || Lax  || Strict
+    ]
+);
+
+// add routers
+$app->add_router('/calabash/user', UserRouter::class);
--- lib/core/Calabash.php
+++ lib/core/Calabash.php
@@ -40,4 +40,29 @@ class Calabash {
         // add router
         $this->router->add($base_path, $router_class);
     }
+
+    /**
+     * Sets cookies
+     * @param string $name The name of the cookie.
+     * @param string $value [optional] The value of the cookie. This value is stored on the clients computer; 
+     *                      do not store sensitive information. Assuming the name is 'cookiename', 
+     *                      this value is retrieved through $_COOKIE['cookiename']
+     * @param array $options [optional] An associative array which may have any of the keys expires, path, 
+     *                      domain, secure, httponly and samesite. The values have the same meaning as described 
+     *                      for the parameters with the same name. The value of the samesite element should be 
+     *                      either Lax or Strict. If any of the allowed options are not given, their default 
+     *                      values are the same as the default values of the explicit parameters. If the samesite 
+     *                      element is omitted, no SameSite cookie attribute is set.
+     * @return bool If output exists prior to calling this function, setcookie will fail and return false. If 
+     *              setcookie successfully runs, it will return true. This does not indicate whether the user 
+     *              accepted the cookie.
+     * @link https://php.net/manual/en/function.setcookie.php
+     */
+    public function set_cookies(string $name, string $value = "", array $options = []): bool {
+        return setcookie(
+            $name,
+            $value,
+            $options
+        );
+    }
 }
